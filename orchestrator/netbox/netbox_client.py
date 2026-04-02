42377d5687fd39dc5cde9de1f7894c7d944dc7fc
PaulyD <arbaz.shaik23@gmail.com>
2026-04-02 00:19:19 +0000

    updates

---
+++ orchestrator/netbox/netbox_client.py
@@ -0,0 +1,48 @@
+import pynetbox
+
+NETBOX_URL = "http://10.10.116.8:8000"
+NETBOX_TOKEN = "2Iz6D0gjvTiitkPe3pJYehX7tubUmfJXHbpe7Q57"
+
+nb = pynetbox.api(NETBOX_URL, token=NETBOX_TOKEN)
+
+def get_or_create_site(name):
+    site = nb.dcim.sites.get(name=name)
+    if not site:
+        site = nb.dcim.sites.create({
+            "name": name,
+            "slug": name.lower().replace(" ", "-")
+        })
+    return site
+
+def get_or_create_device(name, site, role="router"):
+    device = nb.dcim.devices.get(name=name)
+    if not device:
+        device = nb.dcim.devices.create({
+            "name": name,
+            "site": site.id,
+            "device_role": 1,   # adjust later
+            "device_type": 1    # adjust later
+        })
+    return device
+
+def create_interface(device, name):
+    iface = nb.dcim.interfaces.get(device_id=device.id, name=name)
+    if not iface:
+        iface = nb.dcim.interfaces.create({
+            "device": device.id,
+            "name": name,
+            "type": "virtual"
+        })
+    return iface
+
+def create_ip(address, interface):
+    ip = nb.ipam.ip_addresses.get(address=address)
+    if not ip:
+        ip = nb.ipam.ip_addresses.create({
+            "address": address,
+            "status": "active"
+        })
+        ip.assigned_object_id = interface.id
+        ip.assigned_object_type = "dcim.interface"
+        ip.save()
+    return ip
